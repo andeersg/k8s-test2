e66c1da3de6a44e8153092e47475e6adf5f63f7b
Anders Grendstadbakk <anders.2205@gmail.com>
2022-07-06 22:59:20 +0200

    Add styles

--- web/index.php
+++ web/index.php
@@ -5,6 +5,12 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <style>
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  </style>
 </head>
 <body>
   <h1><?php echo "Hello there, i'm PHP"; ?></h1>
